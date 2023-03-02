433210292cfe73e22f6a171cf6a848e2d18a1320
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-03-02 13:19:21 -0500

    please work in npm publish

--- src/character-card.js
+++ src/character-card.js
@@ -16,19 +16,29 @@ export class CharacterCard extends LitElement {
       accentColor: {
         type: String,
         reflect: true,
-        attribute: 'accent-color'
+        attribute: "accent-color",
       },
       meme: {
-        type: String
+        type: String,
       },
       topText: {
-        type: String
-      }
+        type: String,
+      },
+      opened: {
+        type: Boolean,
+        reflect: true,
+      },
     };
   }
 
   static get styles() {
     return css`
+      :host {
+        display: inline-block;
+        vertical-align: text-top;
+
+      }
+
       :host([accent-color="orange"]) .container {
         background-color: var(--character-card-accent-color, orange);
         color: white;
@@ -41,10 +51,19 @@ export class CharacterCard extends LitElement {
         background-color: var(--character-card-accent-color, red);
         color: white;
       }
+
+      .container {
+        background-color: orange;
+        padding: 16px;
+        border-radius: 8px;
+        vertical-align: top;
+      }
+
       .wrapper {
         text-align: center;
         max-width: 400px;
         margin: 16px;
+        vertical-align: top;
       }
 
       .image {
@@ -70,18 +89,6 @@ export class CharacterCard extends LitElement {
         font-weight: bold;
       }
 
-      @media only screen and (max-width: 500px) {
-        .details {
-          display: none;
-        }
-      }
-
-      @media only screen and (min-width: 800px) {
-        .details {
-          display: none;
-        }
-      }
-
       @media screen and (max-width: 500px) {
         .wrapper {
           width: 300px;
@@ -94,144 +101,73 @@ export class CharacterCard extends LitElement {
           font-size: 16px;
         }
       }
-
-      .buttons button:hover {
-        background-color: pink;
-        color: red;
-      }
-
-      .buttons button:focus {
-        background-color: green;
-      }
-
-      .container {
-        background-color: orange;
-        padding: 16px;
-        border-radius: 8px;
-      }
     `;
   }
 
   constructor() {
     super();
-    this.meme = "https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600%2Cc_limit/Ideas_Surprised_Pikachu_HD.jpg";
-    this.accentColor = null;
     this.characterName = "Pikachu";
     this.characterBio =
       "Pikachu, the Mouse Pokémon. It can generate electric attacks from " +
       " the electric pouches located in both of its cheeks.";
     this.topText = "When you forgot to do the weekly assignments";
+    this.opened = false;
   }
 
-  toggleDetails() {
-    this.shadowRoot.querySelector(".details").toggleAttribute("open");
+  toggleEvent() {
+    const state =
+      this.shadowRoot.querySelector("details").getAttribute("open") === ""
+        ? true
+        : false;
+    this.opened = state;
+    console.log(this.opened);
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === "opened") {
+        this.dispatchEvent(
+          new CustomEvent("opened-changed", {
+            composed: true,
+            bubbles: true,
+            cancelable: false,
+            detail: { value: this[propName] },
+          })
+        );
+        console.log(`${propName} changed. oldValue: ${oldValue}`);
+      }
+    });
   }
 
   render() {
     return html`
       <div class="wrapper">
         <div class="container">
+          <!-- Header -->
           <div class="header">
             <h4>${this.characterName}</h4>
           </div>
+
+          <!-- Image -->
           <img class="image" src="${characterImage}" />
-          <details class="details">
-            <slot></slot>
+
+          <!-- Details -->
+          <details
+            class="details"
+            .open="${this.opened}"
+            @toggle="${this.toggleEvent}"
+          >
+            <summary>Click to see more</summary>
             <p>${this.characterBio}</p>
-            <meme-maker
-              alt="suprised pikachu"
-              image-url="${this.meme}"
-              top-text="${this.topText}"
-            ></meme-maker>
+            <slot></slot>
           </details>
-          <my-tag background-color="orange"></my-tag>
+
+          <!-- meme maker -->
+          <card-meme-maker></card-meme-maker>
         </div>
       </div>
     `;
   }
 }
 
-class MyTag extends HTMLElement {
-  /**
-   * This is a convention, not the standard
-   */
-  static get tag() {
-    return "my-tag";
-  }
-  /**
-   * object life cycle
-   */
-  constructor() {
-    super();
-    this.backgroundColor = "orange";
-    // create a template element for processing shadowRoot
-    this.template = document.createElement("template");
-    // create a shadowRoot
-    this.attachShadow({ mode: "open" });
-    this.render();
-  }
-  // render HTML
-  get html() {
-    return `
-      <style>
-        :host {
-          display: inline-block;
-          color: blue;
-          background-color: ${this.backgroundColor};
-        }
-      </style>
-    `;
-  }
-  /**
-   * life cycle, element is afixed to the DOM
-   */
-  connectedCallback() {
-    if (window.ShadyCSS) {
-      window.ShadyCSS.styleElement(this);
-    }
-    this.addEventListener("click", () => {
-      if (this.backgroundColor === "orange") {
-        this.backgroundColor = "yellow";
-      } else {
-        this.backgroundColor = "orange";
-      }
-      this.render();
-    });
-  }
-  /**
-   * Render / rerender the shadowRoot
-   */
-  render() {
-    this.dispatchEvent(
-      new CustomEvent("rerender-mytag-please", {
-        bubbles: true,
-        composed: true,
-        cancelable: true,
-        detail: {
-          value: this.text,
-        },
-      })
-    );
-    this.shadowRoot.innerHTML = null;
-    this.template.innerHTML = this.html;
-    if (window.ShadyCSS) {
-      window.ShadyCSS.prepareTemplate(this.template, this.tag);
-    }
-    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
-  }
-  /**
-   * attributes to notice changes to
-   */
-  static get observedAttributes() {
-    return ["text"];
-  }
-  set text(val) {
-    this.setAttribute("text", val);
-  }
-  get text() {
-    return this.getAttribute("text");
-  }
-}
-
-customElements.define(MyTag.tag, MyTag);
 customElements.define("character-card", CharacterCard);
